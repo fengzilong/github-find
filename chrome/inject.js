01ae5cbba3beff116df913a073061e986024ad15
mo <feng_zilong@163.com>
2016-09-04 22:26:42 +0800

    Fixed: lint errors

--- chrome/inject.js
+++ chrome/inject.js
@@ -1,34 +1,29 @@
 var ArrayProto = Array.prototype;
-
-
 var HIGHLIGHT_WRAPPER_CLASS = '__highlight_wrapper__';
 var HIGHLIGHT_CLASS = '__highlight__';
 var HIGHLIGHT_CURRENT_CLASS = '__highlight_current__';
-
-function walk(rootNode, text){
-	ArrayProto.forEach.call(rootNode.childNodes, function(node, i){
-		if( node.nodeType === 3 && node.nodeValue.indexOf(text) >= 0 ){
-			nodes.push(node);
-		} else {
-			if(node.childNodes.length > 0){
-				walk(node, text);
-			}
-		}
-	})
-}
-
 var nodes = [];
 
-function hightlight(text){
+function walk(rootNode, text) {
+	ArrayProto.forEach.call(rootNode.childNodes, function (node) {
+		if (node.nodeType === 3 && node.nodeValue.indexOf(text) >= 0) {
+			nodes.push(node);
+		} else if (node.childNodes.length > 0) {
+			walk(node, text);
+		}
+	});
+}
+
+function hightlight(text) {
 	walk(document.querySelector('.blob-wrapper'), text);
 
-	nodes.forEach(function(node){
+	nodes.forEach(function (node) {
 		var parentNode = node.parentNode;
 
 		var tmp = document.createElement('div');
 		tmp.innerHTML = '<span class="' + HIGHLIGHT_WRAPPER_CLASS + '">' + node.nodeValue.replace(new RegExp('(' + text.replace(/\(/g, '\\(').replace(/\)/g, '\\)') + ')', 'g'), '<span class="' + HIGHLIGHT_CLASS + '">$1</span>') + '</span>';
 
-		while(tmp.firstChild){
+		while (tmp.firstChild) {
 			parentNode.insertBefore(tmp.firstChild, node);
 		}
 
@@ -38,17 +33,17 @@ function hightlight(text){
 	nodes = [];
 }
 
-function cancelHighLight(){
+function cancelHighLight() {
 	var hightlightWrappers = document.querySelectorAll('.' + HIGHLIGHT_WRAPPER_CLASS);
 
-	ArrayProto.forEach.call(hightlightWrappers, function(wrapper){
+	ArrayProto.forEach.call(hightlightWrappers, function (wrapper) {
 		var parentNode = wrapper.parentNode;
-		
+
 		var text = wrapper.textContent;
 		var tmp = document.createElement('div');
 		tmp.innerHTML = text;
 
-		while(tmp.firstChild){
+		while (tmp.firstChild) {
 			parentNode.insertBefore(tmp.firstChild, wrapper);
 		}
 
@@ -56,28 +51,31 @@ function cancelHighLight(){
 	});
 }
 
-function injectCss(cssContent){
+function injectCss(cssContent) {
 	var stylesheet = document.createElement('style');
 	stylesheet.type = 'text/css';
 	stylesheet.textContent = cssContent;
 	document.head.appendChild(stylesheet);
-};
+}
 
-function centerOffset(elm){
+function centerOffset(elm) {
 	var rect = elm.getBoundingClientRect();
 
 	return {
-		top: rect.top + document.body.scrollTop + elm.offsetHeight / 2,
-		left: rect.left + document.body.scrollLeft + elm.offsetWidth / 2
+		top: rect.top + document.body.scrollTop + (elm.offsetHeight / 2),
+		left: rect.left + document.body.scrollLeft + (elm.offsetWidth / 2),
 	};
 }
 
-function findNearestHighlightNode(top, left){
-	var elms = ArrayProto.slice.call(document.querySelectorAll('.' + HIGHLIGHT_CLASS)),
-		distances = [],
+function findNearestHighlightNode(top, left) {
+	var elms,
+		distances,
 		minDistance;
-	
-	elms.forEach(function( v, i ){
+
+	elms = ArrayProto.slice.call(document.querySelectorAll('.' + HIGHLIGHT_CLASS));
+	distances = [];
+
+	elms.forEach(function (v) {
 		var o = centerOffset(v) || {};
 		var t = o.top || -10000;
 		var l = o.left || -10000;
@@ -91,31 +89,30 @@ function findNearestHighlightNode(top, left){
 }
 
 document.addEventListener('mouseup', function (e) {
-	var words = window.getSelection().toString().trim(),
+	var words,
 		current;
 
-	if(e.button === 2 || words === ''){
+	words = window.getSelection().toString().trim();
+
+	if (e.button === 2 || words === '') {
 		return;
 	}
-	
+
 	cancelHighLight();
 	hightlight(words);
 
 	current = findNearestHighlightNode(e.pageY, e.pageX);
 
-	//shall call insertCss
-	injectCss('.' + HIGHLIGHT_CLASS + '{background-color: #FF0;}' + '.' + HIGHLIGHT_CURRENT_CLASS + '::selection{background-color: #FF9632;}');
+	// shall call insertCss
+	injectCss('.' + HIGHLIGHT_CLASS + '{background-color: #FF0;} .' + HIGHLIGHT_CURRENT_CLASS + '::selection{background-color: #FF9632;}');
 
-	//set selection
-	if( current ){
+	// set selection
+	if (current) {
 		current.classList.add(HIGHLIGHT_CURRENT_CLASS);
-		
-		var node = current.firstChild;
+
 		var range = document.createRange();
 		range.selectNodeContents(current);
-		//range.setStart(node, 0);
-		//range.setEnd(node, node.textContent.length);
-		
+
 		var selection = window.getSelection();
 		selection.removeAllRanges();
 		selection.addRange(range);
@@ -124,10 +121,9 @@ document.addEventListener('mouseup', function (e) {
 	}
 }, false);
 
-document.addEventListener('keydown', function (e) {
-	//ESC
-	if(e.keyCode === 27){
+document.addEventListener('keydown', function onKeyDown(e) {
+	// ESC
+	if (e.keyCode === 27) {
 		cancelHighLight();
 	}
 }, false);
-
